673a5c1df382045520d5e85f62b67adb57022532
Ruthressh21 <ruthresshvikneswaran@gmail.com>
2022-08-19 18:29:49 +0800

    Ruth

---
+++ app/src/main/java/my/edu/blooddonationapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package my.edu.blooddonationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.replace
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/my/edu/blooddonationapp/userLoginFragment.kt
@@ -0,0 +1,19 @@
+package my.edu.blooddonationapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class userLoginFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    }
+
+
